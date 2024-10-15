d0964f9504ebbdd6d11182fed7de28113c5de6b5
Yiğit <yigitcanbey4@gmail.com>
2024-10-15 23:51:27 +0300

    ileri seviye veri yapıları ve objeler

---
+++ SıfırdanİleriPython/İleriSeviyeVeriYapılarıveObjeler/ileri_seviye_sayılar.py
@@ -0,0 +1,16 @@
+print(bin(19)) # 'bin' 2'lik tabanda yazar. Çıktı: 10011
+print(hex(19)) # 'hex' 16'lık tabanda yazar. Çıktı: 13
+
+# fonksiyonlar
+
+print(abs(-4)) # 'abs' bir sayının mutlak değerini alır.
+
+print(round(4.8)) # sayıyı yuvarlar. 4.5'i 4'e yuvarlar.
+print(round(3.2229,3)) # verdiğimiz 3 değeri noktadan sonraki 3. basamağa kadar yuvarlar.
+
+print(max(3,4,5,6,1,-2)) # en büyük sayıyı döndürür.
+print(min(1,2,3,10,9)) # en küçük sayıyı döndürür.
+
+print(sum([1,2,3,4,5,6])) # ekrana listedeki sayıların toplamını döndürür. İçine string gönderme.
+
+print(pow(2,4)) # 2'nin 4. kuvvetini alır.
